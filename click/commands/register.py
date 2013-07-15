5674d027d8ef3470b2e59edfe7b461e89b3faa82
Colin Watson <cjwatson@ubuntu.com>
2013-07-15 15:55:48 +0100

    0.1.4 (patches unapplied)
    Imported using git-ubuntu import.

--- click/commands/register.py
+++ click/commands/register.py
@@ -13,37 +13,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Install or remove a Click system hook."""
+"""Register an installed Click package for a user."""
 
 from __future__ import print_function
 
 from optparse import OptionParser
 
-from click.hooks import ClickHook
 from click.paths import default_root
-
-
-subcommands = {
-    "install": "install_all",
-    "remove": "remove_all",
-    }
+from click.user import ClickUser
 
 
 def run(argv):
-    parser = OptionParser("%prog hook [options] {install|remove} HOOK")
+    parser = OptionParser("%prog register [options] PACKAGE-NAME VERSION")
     parser.add_option(
         "--root", metavar="PATH", default=default_root,
         help="set top-level directory to PATH (default: %s)" % default_root)
+    parser.add_option(
+        "--user", metavar="USER",
+        help="register package for USER (default: current user)")
     options, args = parser.parse_args(argv)
     if len(args) < 1:
-        parser.error("need subcommand (install, remove)")
-    subcommand = args[0]
-    if subcommand not in subcommands:
-        parser.error(
-            "unknown subcommand '%s' (known: install, remove)" % subcommand)
+        parser.error("need package name")
     if len(args) < 2:
-        parser.error("need hook name")
-    name = args[1]
-    hook = ClickHook.open(name)
-    getattr(hook, subcommands[subcommand])(options.root)
-    return 0
+        parser.error("need version")
+    package = args[0]
+    version = args[1]
+    registry = ClickUser(options.root, user=options.user)
+    registry[package] = version
